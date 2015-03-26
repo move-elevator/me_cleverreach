49bda38ca2d308a769c7d1688bdfc2d29f62874a
Steve Schütze <sts@me-office365.de>
2015-03-26 12:24:32 +0100

    code style updates

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,4 +26,3 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(),
 	),
 );
-?>
--- ext_tables.php
+++ ext_tables.php
@@ -11,6 +11,4 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('extbase')) {
 	$pluginSignature = strtolower($extensionName) . '_cleverreach';
 	$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
 	$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
-
 }
