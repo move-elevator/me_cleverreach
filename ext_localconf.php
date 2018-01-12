6d4839f0dc6f35e506171f9569440b9fe64f5e7d
Tim Lochmüller <tim.lochmueller@hdnet.de>
2018-01-12 15:43:36 +0100

    Drop useless call to addPItoST43

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,6 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY);
 
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('extbase')) {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('MoveElevator.' . $_EXTKEY, 'MeCleverReach',
