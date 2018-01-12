2373e77bbe7fb1fc6b594ddf6d2eb5400a9c6fc0
Tim Lochmüller <tim.lochmueller@hdnet.de>
2018-01-12 15:44:05 +0100

    Drop useless extbase check - extbase is always active

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,9 +4,7 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('extbase')) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('MoveElevator.' . $_EXTKEY, 'MeCleverReach',
-        array('Subscribe' => 'subscribeForm,subscribe'),
-        array('Subscribe' => 'subscribeForm,subscribe')
-    );
-}
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('MoveElevator.' . $_EXTKEY, 'MeCleverReach',
+    array('Subscribe' => 'subscribeForm,subscribe'),
+    array('Subscribe' => 'subscribeForm,subscribe')
+);
--- ext_tables.php
+++ ext_tables.php
@@ -4,10 +4,8 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('extbase')) {
-    $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'MeCleverReach', 'm:e CleverReach');
-    $pluginSignature = strtolower($extensionName) . '_cleverreach';
-    $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-    $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-}
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'MeCleverReach', 'm:e CleverReach');
+$pluginSignature = strtolower($extensionName) . '_cleverreach';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
