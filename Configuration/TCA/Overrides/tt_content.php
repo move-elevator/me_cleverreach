fce7c35ecb4d689782e6dc8433ad067bd3ace03b
Tim Lochmüller <tim.lochmueller@hdnet.de>
2018-01-12 15:45:48 +0100

    Move TCA to TCA folder

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,5 @@
+<?php
+
+$pluginSignature = 'mecleverreach_cleverreach';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'MeCleverReach', 'm:e CleverReach');
-$pluginSignature = strtolower($extensionName) . '_cleverreach';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
